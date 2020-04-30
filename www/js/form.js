b2f67fa08ffae7a8aa55fba5e6f62e0266100441
mohammed almajid <hamodey85@gmail.com>
2020-04-30 23:10:48 +1000

    remove useless js links and console log form values

--- www/js/form.js
+++ www/js/form.js
@@ -1,15 +1,26 @@
 const popUpModule = document.getElementById('pop_up_container')
 const closeIcon = document.getElementById('close_icon')
 const form = document.getElementById('form')
+const values = {}
+let temp  ;
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  console.log(new FormData(form))
-  popUpModule.classList.remove('hideModal')
-})
-closeIcon.addEventListener('click', (e) => {
-  popUpModule.classList.add('hideModal')
-})
 popUpModule.addEventListener('click', (e) => {
   popUpModule.classList.add('hideModal')
 })
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  
+for(let i=0;i<form.length -1;i++) {
+
+  if( form[i].value !== '' && i === form.length -2) popUpModule.classList.remove('hideModal')
+  temp = form[i].name
+  values[temp]= form[i].value;
+}
+console.table(values)
+})
+// closeIcon.addEventListener('click', (e) => {
+//   popUpModule.classList.add('hideModal')
+// })
+
+
